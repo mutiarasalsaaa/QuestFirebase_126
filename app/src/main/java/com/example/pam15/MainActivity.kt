26901da92c402a3b1f29febe203b72ab7b110cce
mutiara126 <salsamutiara1812@gmail.com>
2025-01-13 02:49:09 +0700

    mengatur antarmuka awal dan navigasi aplikasi saat dijalankan

--- app/src/main/java/com/example/pam15/MainActivity.kt
+++ app/src/main/java/com/example/pam15/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.pam15.ui.page.HomeScreen
+import com.example.pam15.ui.View.HomeScreen
 import com.example.pam15.ui.theme.PAM15Theme
 
 class MainActivity : ComponentActivity() {
@@ -31,18 +31,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PAM15Theme {
-        Greeting("Android")
-    }
-}
