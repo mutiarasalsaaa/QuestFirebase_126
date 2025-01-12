9fd3dd2443ccb3f24c3b9f106e9895e092026451
mutiara126 <salsamutiara1812@gmail.com>
2025-01-13 02:51:07 +0700

    mempermudah pengembangan dan pengecekan desain

--- app/src/main/java/com/example/pam15/MainActivity.kt
+++ app/src/main/java/com/example/pam15/MainActivity.kt
@@ -39,3 +39,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PAM15Theme {
+        Greeting("Android")
+    }
+}
